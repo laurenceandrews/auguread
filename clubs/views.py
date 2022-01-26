61f3b9dab9935858cefc14e56675a5b2d2a916ea
k20065945-suryabrunton <surya.brunton@kcl.ac.uk>
2022-01-26 09:29:16 -0400

    Create basic sign up page

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import SignUpForm
+from .helpers import login_prohibited
+
+
+@login_prohibited
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('feed')
+    else:
+        form = SignUpForm()
+    return render(request, 'sign_up.html', {'form': form})
