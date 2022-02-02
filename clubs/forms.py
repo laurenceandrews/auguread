8c83f7e51f1a376359e9a319ccf36a5c1b5ed39e
Clara Zard <clara.zard@kcl.ac.uk>
2022-02-02 14:23:01 +0000

    Create new password mixin

--- clubs/forms.py
+++ clubs/forms.py
@@ -59,7 +59,32 @@ class SignUpForm(forms.ModelForm):
         )
         return user
 
-class PasswordForm():
+class NewPasswordMixin(forms.Form):
+    """Form mixin for new_password and password_confirmation fields."""
+
+    new_password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+        validators=[RegexValidator(
+            regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+            message='Password must contain an uppercase character, a lowercase '
+                    'character and a number'
+            )]
+    )
+    password_confirmation = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput())
+
+    def clean(self):
+        """ Ensure that new_password and password_confirmation contain the same password."""
+
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation',
+                           'Confirmation does not match password.')
+
+class PasswordForm(NewPasswordMixin):
     """Form enabling users to change their password."""
 
     password = forms.CharField(
