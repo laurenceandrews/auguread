8bfcf844ce33bd3ad607ef591a6108e6992b126b
Clara Zard <clara.zard@kcl.ac.uk>
2022-02-02 15:27:00 +0000

    Sample tests for password view

--- clubs/forms.py
+++ clubs/forms.py
@@ -19,46 +19,6 @@ class LogInForm(forms.Form):
             user = authenticate(email=email, password=password)
         return user
 
-class SignUpForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'bio']
-        widgets = {'bio': forms.Textarea()}
-
-    new_password = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput(),
-        validators=[
-            RegexValidator(
-                regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
-                message='Password must contain an uppercase character, a lowercase character and a number.'
-            ),
-        ],
-    )
-    password_confirmation = forms.CharField(
-        label='Confirm password',
-        widget=forms.PasswordInput(),
-    )
-
-    def clean(self):
-        super().clean()
-        new_password = self.cleaned_data.get('new_password')
-        password_confirmation = self.cleaned_data.get('password_confirmation')
-        if new_password != password_confirmation:
-            self.add_error('password_confirmation', 'Confirmation does not match password.')
-
-    def save(self):
-        super().save(commit=False)
-        user = User.objects.create_user(
-            self.cleaned_data.get('username'),
-            first_name=self.cleaned_data.get('first_name'),
-            last_name=self.cleaned_data.get('last_name'),
-            email=self.cleaned_data.get('email'),
-            bio=self.cleaned_data.get('bio'),
-            password=self.cleaned_data.get('new_password'),
-        )
-        return user
-
 class NewPasswordMixin(forms.Form):
     """Form mixin for new_password and password_confirmation fields."""
 
@@ -116,3 +76,43 @@ class PasswordForm(NewPasswordMixin):
             self.user.set_password(new_password)
             self.user.save()
         return self.user
+
+class SignUpForm(NewPasswordMixin, forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'bio']
+        widgets = {'bio': forms.Textarea()}
+
+    new_password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+        validators=[
+            RegexValidator(
+                regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+                message='Password must contain an uppercase character, a lowercase character and a number.'
+            ),
+        ],
+    )
+    password_confirmation = forms.CharField(
+        label='Confirm password',
+        widget=forms.PasswordInput(),
+    )
+
+    def clean(self):
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation', 'Confirmation does not match password.')
+
+    def save(self):
+        super().save(commit=False)
+        user = User.objects.create_user(
+            self.cleaned_data.get('username'),
+            first_name=self.cleaned_data.get('first_name'),
+            last_name=self.cleaned_data.get('last_name'),
+            email=self.cleaned_data.get('email'),
+            bio=self.cleaned_data.get('bio'),
+            password=self.cleaned_data.get('new_password'),
+        )
+        return user
