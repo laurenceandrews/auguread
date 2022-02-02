90b00c2db43d0e1fc6cb9f0fce045ce7ece391a1
Clara Zard <clara.zard@kcl.ac.uk>
2022-02-02 18:49:55 +0000

    Add some tests for show user view

--- clubs/tests/helpers.py
+++ clubs/tests/helpers.py
@@ -1,4 +1,6 @@
 from django.urls import reverse
+from clubs.models import Post
+from with_asserts.mixin import AssertHTMLMixin
 
 
 def reverse_with_next(url_name, next_url):
@@ -6,6 +8,12 @@ def reverse_with_next(url_name, next_url):
     url += f"?next={next_url}"
     return url
 
+def create_posts(author, from_count, to_count):
+    """Create unique numbered posts for testing purposes."""
+    for count in range(from_count, to_count):
+        text = f'Post__{count}'
+        post = Post(author=author, text=text)
+        post.save()
 
 class LogInTester:
     def _is_logged_in(self):
