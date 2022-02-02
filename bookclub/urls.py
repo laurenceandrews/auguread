f3e839424798e76c173ef8eb7328b29fade1fb86
Clara Zard <clara.zard@kcl.ac.uk>
2022-02-02 18:26:07 +0000

    Add urls for show user and user list

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('log_in/', views.LogInView.as_view(), name='log_in'),
     path('log_out/', views.log_out, name='log_out'),
     path('password/', views.PasswordView.as_view(), name='password'),
+    path('user/<int:user_id>', views.ShowUserView.as_view(), name='show_user'),
+    path('users/', views.UserListView.as_view(), name='user_list'),
 ]
