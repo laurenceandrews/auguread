c74e892e2574855450ac8de410c2d2e4610331e7
YasameenSMohammed <yasameen.mohammed@kcl.ac.uk>
2022-02-07 21:15:20 -0400

    Add Club_Books junction table

--- clubs/management/commands/club_seeder.py
+++ clubs/management/commands/club_seeder.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
-from clubs.models import Club, User, Club_Users, Club_Books
+from clubs.models import Club, User, Book, Club_Users, Club_Books
 import pandas as pd
 import random
 from random import randint
@@ -10,12 +10,13 @@ class Command(BaseCommand):
 
     HOW_MANY_CLUBS_TO_MAKE = 10
     HOW_MANY_USERS_TO_ADD = 100
+    HOW_MANY_BOOKS_TO_ADD = 100
     USER_ID = 0
     first_name = ""
     last_name = ""
 
     # get users from the database separated into columns (using pandas)
-    def read_from_file(self):
+    def read_users_from_file(self):
         columns = ["id", "Location", "Age"]
         user_data = pd.read_csv(
             r'clubs/dataset/BX-Users.csv',
@@ -30,21 +31,47 @@ class Command(BaseCommand):
         )
         return user_data
 
+     # get books from the database separated into columns (using pandas)
+    def read_books_from_file(self):
+        columns = ["ISBN", "Book-Title", "Book-Author", "Year-Of-Publication", "Publisher", "Image-URL-S", "Image-URL-M", "Image-URL-L"]
+        book_data = pd.read_csv(
+            r'clubs/dataset/BX-Books.csv',
+            encoding='Latin-1',
+            delimiter=';',
+            names=columns,
+            dtype={
+                "ISBN": "string",
+                "Book-Title": "string",
+                "Book-Author": "string",
+                # "Year-Of-Publication" : "integer",
+                "Publisher" : "string",
+                "Image-URL-S" : "string",
+                "Image-URL-M" : "string",
+                "Image-URL-L" : "string",
+            }
+        )
+        return book_data   
+
     def __init__(self):
         super().__init__()
         self.faker = Faker('en_GB')
 
         self.file1_append = open("seededClubs.txt", "a")
         self.file2_append = open("seededUsers.txt", "a")
+        self.file3_append = open("seededBooks.txt", "a")
 
         self.clubs_made = []
         self.club_count = 0
         self.users_made = []
         self.user_count = 0
+        self.books_made = []
+        self.book_count = 0
 
     def __del__(self):
         self.file1_append.close()
         self.file2_append.close()
+        self.file3_append.close()
+
 
     # took some code from our old seed.py file from grasshopper
     def create_club(self):
@@ -65,7 +92,7 @@ class Command(BaseCommand):
         # If the club name doesn't already exist
         if not Club.objects.filter(name=club_name).exists():
             # Generate random club fields
-            club_location = self.generate_random_location()
+            club_location = self.get_random_location()
             club_description = self.faker.text(max_nb_chars=520)
             club_reading_speed = random.randint(50, 500)
             
@@ -99,16 +126,48 @@ class Command(BaseCommand):
             self.clubs_made.append(club)
             self.club_count += 1
 
+            # Assigning favourite books to club
+            fav_books = random.choices(Book.objects.all(), k = 5)
+            for book in fav_books:
+                fav_book = Club_Books.objects.create(
+                    club_id = club,
+                    book_id = book
+                )
+                fav_book.save()
+
+
+    def create_book(self):
+        ISBN = self.faker.isbn13()
+        book = Book.objects.create(
+                ISBN = ISBN,
+                title = self.faker.text(max_nb_chars=20),
+                author = self.faker.name(),
+                publisher = self.faker.company(),
+                publication_year = self.faker.year()
+            )
+        book.save()
+
+        # Append the new book name to the file
+        self.file1_append.write(ISBN + "\n")
+
+        self.books_made.append(book)
+        self.book_count += 1
+
     # get a random user id from the list of users in the dataset
     def get_random_user(self):
-        ids = self.read_from_file().id.to_list()
+        ids = self.read_users_from_file().id.to_list()
         random.choice(ids)
 
+    # get random ISBNs from the list of books in the dataset (currently chooses 5)
+    def get_random_books(self):
+        books = self.read_books_from_file().ISBN.to_list()
+        random.choice(books)    
+
     # generate a random location from a made-up list (can also do it with the user locations but we would have to format them first)
-    def generate_random_location(self):
+    def get_random_location(self):
         locations = ["London", "Manchester", "Birmingham",
                      "Brighton", "Bristol", "Online", "Glasgow", "USA"]
-        return random.choice(locations)
+        return random.choice(locations)   
 
     def seed_user_in_club(self):
         user_first_name = self.faker.first_name()
@@ -145,7 +204,15 @@ class Command(BaseCommand):
 
             self.user_count += 1
 
+          
+
     def handle(self, *args, **options):
+
+        while self.book_count < Command.HOW_MANY_BOOKS_TO_ADD:
+            print(f'Seeding books...',  end='\r')
+            self.create_book()
+        print('Finished adding books to clubs')   
+
         while self.club_count < self.HOW_MANY_CLUBS_TO_MAKE:
             print(f'Seeding clubs...',  end='\r')
             self.create_club()
--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -1,11 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
-from clubs.models import User, Book
+from clubs.models import User
 
 class Command(BaseCommand):
     PASSWORD = "Password123"
     USER_COUNT = 100
-    BOOK_COUNT = 100
 
     def __init__(self):
         super().__init__()
@@ -22,16 +21,6 @@ class Command(BaseCommand):
             user_count += 1
         print('User seeding complete')
 
-    def handle(self, *args, **options):
-        book_count = 0
-        while book_count < Command.BOOK_COUNT:
-            print(f'Seeding book {book_count}',  end='\r')
-            try:
-                self._create_book()
-            except (django.db.utils.IntegrityError):
-             continue
-            book_count += 1
-        print('Book seeding complete') 
 
     def _create_user(self):
         first_name = self.faker.first_name()
@@ -55,17 +44,3 @@ class Command(BaseCommand):
     def _username(self, first_name, last_name):
         username = f'@{first_name}{last_name}'
         return username
-
-    def _create_book(self):
-        ISBN = self.faker.isbn13()
-        title = self.faker.text(max_nb_chars=20)
-        author = self.faker.name()
-        publisher = self.faker.company()
-        publication_year = self.faker.year()
-        Book.objects.create(
-            ISBN = ISBN,
-            title = title,
-            author = author,
-            publisher = publisher,
-            publication_year = publication_year
-        )
