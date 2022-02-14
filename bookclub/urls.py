617990ca7d1d6a827001b2d1a249bbc8ee98c300
k20065945-suryabrunton <surya.brunton@kcl.ac.uk>
2022-02-14 07:35:03 -0400

    Remove homepage for scheduler

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -35,7 +35,6 @@ urlpatterns = [
     path('new_club/', views.NewClubView.as_view(), name='new_club'),
     path('new_post/', views.NewPostView.as_view(), name='new_post'),
     # sample scheduler
-    url(r'^homepage/', TemplateView.as_view(template_name="homepage.html"), name='homepage'),
     url(r'^schedule/', include('schedule.urls')),
     url(r'^fullcalendar/', TemplateView.as_view(template_name="fullcalendar.html"), name='fullcalendar'),
 ]
