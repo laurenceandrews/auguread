d19cff5d1ae02b7362dfe78aacb47e7baf5af36f
Clara Zard <clara.zard@kcl.ac.uk>
2022-02-24 14:49:56 +0000

    Fix enter view

--- clubs/models.py
+++ clubs/models.py
@@ -49,10 +49,10 @@ class User(AbstractUser):
         ]
     )
 
-    # id = models.CharField(
-    #     unique=True,
-    #     max_length=20
-    # )
+    id = models.CharField(
+        unique=True,
+        max_length=20
+    )
 
     first_name = models.CharField(
         max_length=50,
