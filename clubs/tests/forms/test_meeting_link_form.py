1349ce079f63a7759797c2c795124f6737d94e68
k20065945-suryabrunton <surya.brunton@kcl.ac.uk>
2022-03-02 18:40:15 +0000

    Create tests for MeetingLink form

---
+++ clubs/tests/forms/test_meeting_link_form.py
@@ -0,0 +1,24 @@
+"""Unit tests of the meeting address form."""
+from clubs.forms import MeetingLinkForm
+from clubs.models import MeetingLink, User
+from django import forms
+from django.contrib.auth.hashers import check_password
+from django.test import TestCase
+from schedule.models import Calendar, Event, Rule
+
+
+class MeetingLinkFormTestCase(TestCase):
+    """Unit tests of the meeting address form."""
+
+    def setUp(self):
+        self.form_input = {
+            'meeting_link': "https://www.bing.com/"
+        }
+
+    def test_valid_meeting_link_form(self):
+        form = MeetingLinkForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_has_necessary_fields(self):
+        form = MeetingLinkForm()
+        self.assertIn('meeting_link', form.fields)
