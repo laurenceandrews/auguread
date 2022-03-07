b7cb946c1105cd91e26dd97b0cd2600fecc534b5
Clara Zard <clara.zard@kcl.ac.uk>
2022-03-07 16:59:09 +0000

    Try to fix test_new_club_form

--- clubs/tests/forms/test_new_club_form.py
+++ clubs/tests/forms/test_new_club_form.py
@@ -24,15 +24,12 @@ class NewClubTestCase(TestCase):
             'country': self.club_country,
             'description': 'A book club that is fun.',
             'avg_reading_speed': 200,
+            # 'meeting_type': 'online',
             'calendar_name': 'Fun Reading Clubs Calendar'
         }
 
-    # def test_valid_new_club(self):
-    #     form = NewClubForm(data=self.form_input)
-    #     self.assertTrue(form.is_valid())
-
     def test_valid_new_club(self):
-        form = NewClubForm(data=input)
+        form = NewClubForm(data=self.form_input)
         self.assertTrue(form.is_valid())
 
     def test_form_has_necessary_fields(self):
@@ -42,6 +39,7 @@ class NewClubTestCase(TestCase):
         self.assertIn('country', form.fields)
         self.assertIn('description', form.fields)
         self.assertIn('avg_reading_speed', form.fields)
+        # self.assertIn('meeting_type', form.fields)
         self.assertIn('calendar_name', form.fields)
 
     def test_name_cannot_exceed_50_characters(self):
