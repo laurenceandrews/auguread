dc85863ff47162ccecd2188101a7378cfb812509
k20065945-suryabrunton <surya.brunton@kcl.ac.uk>
2022-03-14 15:07:57 +0000

    Create tests for event_link_update view

--- clubs/tests/views/test_event_link_update_view.py
+++ clubs/tests/views/test_event_link_update_view.py
@@ -1,18 +1,16 @@
-"""Tests for the event_update view."""
+"""Tests for the event_link_update view."""
 import datetime
 
-from clubs.forms import CreateEventForm
-from clubs.models import Club, User
+from clubs.forms import MeetingLinkForm
+from clubs.models import Club, MeetingLink, User
 from clubs.tests.helpers import reverse_with_next
 from django.test import TestCase
 from django.urls import reverse
 from schedule.models import Calendar, Event, Rule
-from testfixtures import Replace, test_datetime
-from testfixtures.tests.sample1 import str_now_1
 
 
-class EventUpdateViewTest(TestCase):
-    """Test suite for the event_update view."""
+class EventLinkUpdateViewTest(TestCase):
+    """Test suite for the event_link_update view."""
 
     fixtures = [
         'clubs/tests/fixtures/default_user.json',
@@ -37,48 +35,58 @@ class EventUpdateViewTest(TestCase):
         self.event = Event(**data)
         self.event.save()
 
+        self.meeting_link = MeetingLink.objects.create(event=self.event, meeting_link='https://zoom.us/test')
+
         self.url = reverse(
-            'edit_event', kwargs={'calendar_slug': self.calendar.slug, 'event_id': self.event.id}
+            'edit_event_link', kwargs={'calendar_slug': self.calendar.slug, 'event_id': self.event.id}
         )
 
         self.form_input = {
-            'title': 'Pay Mortgage'
+            'meeting_link': 'https://meet.google.com/xxx-yyyy-zzz'
         }
 
-    def test_event_edit_url(self):
-        self.assertEqual(self.url, f'/event/edit/{self.calendar.slug}/{self.event.id}/')
+    def test_event_link_edit_url(self):
+        self.assertEqual(self.url, f'/event/link/edit/{self.calendar.slug}/{self.event.id}/')
 
-    def test_get_event_edit(self):
+    def test_get_event_link_edit(self):
         self.client.login(email=self.user.email, password="Password123")
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'event_update.html')
+        self.assertTemplateUsed(response, 'event_link_update.html')
         form = response.context['form']
-        self.assertTrue(isinstance(form, CreateEventForm))
+        self.assertTrue(isinstance(form, MeetingLinkForm))
         self.assertEqual(form.instance, self.event)
 
-    def test_get_event_edit_redirects_when_not_logged_in(self):
+    def test_get_event_link_edit_redirects_when_not_logged_in(self):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
-    def test_unsuccesful_event_edit_update(self):
+    def test_unsuccesful_event_link_edit_update(self):
         self.client.login(email=self.user.email, password="Password123")
-        self.form_input['title'] = ''
-        before_count = Event.objects.count()
+        self.form_input['meeting_link'] = ''
+        before_count = MeetingLink.objects.count()
         response = self.client.post(self.url, self.form_input)
-        after_count = Event.objects.count()
+        after_count = MeetingLink.objects.count()
         self.assertEqual(after_count, before_count)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'event_update.html')
+        self.assertTemplateUsed(response, 'event_link_update.html')
         form = response.context['form']
-        self.assertTrue(isinstance(form, CreateEventForm))
+        self.assertTrue(isinstance(form, MeetingLinkForm))
         self.assertTrue(form.is_bound)
-        self.event.refresh_from_db()
-        self.assertEqual(self.event.title, 'Exercise')
-        self.assertEqual(self.event.calendar, self.calendar)
+        self.meeting_link.refresh_from_db()
+        self.assertEqual(self.meeting_link.meeting_link, 'https://zoom.us/test')
 
-    def test_post_event_edit_redirects_when_not_logged_in(self):
+    def test_succesful_event_link_update(self):
+        meeting_link = MeetingLink.objects.get(event=self.event)
+        response = self.client.post(self.url, self.form_input, follow=True)
+
+        self.assertEqual(response.status_code, 302)
+
+        meeting_link.refresh_from_db()
+        self.assertEqual(meeting_link.meeting_link, 'https://meet.google.com/xxx-yyyy-zzz')
+
+    def test_post_event_link_edit_redirects_when_not_logged_in(self):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.post(self.url, self.form_input)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
