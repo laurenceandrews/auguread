2a5a6d27b85a7bc8307eaae2e7117c03d6164aeb
Niki Norgren <niki.norgren@protonmail.com>
2022-03-18 11:27:21 +0800

    update dataset and simple layout 1

--- clubs/book-to-user-recommender/book-to-user.py
+++ clubs/book-to-user-recommender/book-to-user.py
@@ -1,4 +1,28 @@
 
 import numpy as np
 import pandas as pd
-## from clubs.models import Book, Book_Rating
+from clubs.models import Book, Book_Rating
+
+
+class BookToUserRecommender:
+    def __init__(self):
+        # load dataset
+
+    ## experimental layout for implementation
+
+    def getUserRatingCount(self):
+        # find the number of ratings made by each user
+
+    def getRatingInfo(self):
+       pass
+
+    def getPopularAuthors(self):
+        # get the top 10 most popular authors
+        pass
+
+    def getBooksByAuthor(self):
+        # get all the books by the favorite author
+        pass
+
+    def get_recommended_books(self):
+        pass
