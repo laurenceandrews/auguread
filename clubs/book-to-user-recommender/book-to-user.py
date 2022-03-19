fc15f740311fa37ab2fdf620aa68665eaa56522e
Niki Norgren <niki.norgren@protonmail.com>
2022-03-19 16:29:14 +0800

    .py implementation of ml model

--- clubs/book-to-user-recommender/book-to-user.py
+++ clubs/book-to-user-recommender/book-to-user.py
@@ -1,28 +1,60 @@
 
 import numpy as np
 import pandas as pd
-from clubs.models import Book, Book_Rating
+from clubs.models import Book, Book_Rating, Club_Books, Club_Users
 
 
 class BookToUserRecommender:
     def __init__(self):
+      
         # load dataset
 
-    ## experimental layout for implementation
+        self.df_books = pd.DataFrame(list(Book.objects.all().values()))
+        self.df_users = pd.DataFrame(list(Club_Users.objects.all().values()))
+        self.df_ratings = pd.DataFrame(list(Book_Rating.objects.all().values()))
 
-    def getUserRatingCount(self):
-        # find the number of ratings made by each user
+        # removing 0-rating from dataframe
+        self.df_book_ratings.drop(self.df_book_ratings[self.df_book_ratings['rating'] == 0].index, inplace=True) 
 
-    def getRatingInfo(self):
-       pass
+        # columns to keep 
+        self.df_books_cleaned = pd.DataFrame(self.df_books[['ISBN','title', 'author','publication_year','publisher']])
 
-    def getPopularAuthors(self):
-        # get the top 10 most popular authors
+        # removing nan's 
+        self.df_books_cleaned =  self.df_books_cleaned.dropna(how='any', inplace=True)
+
+        # save books that are published between 1950 to 2016
+        self.df_books_cleaned = self.df_books_cleaned[(self.df_books_cleaned['publication_year'] > 1950) & (self.df_books_cleaned['publication_year'] <= 2016)]
+
+        # regex-based replacement of certain characters
+        self.df_books_cleaned['author'] = self.df_books_cleaned['author'].str.replace(r'[^\w\s]+', '')
+
+
+    # experimental layout for implementation
+
+
+    def set_dataframe_restraints(self):
+        # place above df restraints in setter's
+
+
+     def set_age_constraint(self):
+         self.df_users_cleaned = pd.DataFrame(self.df_users[self.df_users['age']<=100])
+
+
+    def get_top_books(self):
+        # get the top 20 highest rated books
         pass
 
-    def getBooksByAuthor(self):
-        # get all the books by the favorite author
+
+    def get_top_authors(self):
+        # get the top 20 most popular authors
         pass
+
+
+    def get_user_prediction(self):
+        # get the prediction of book to recommend
+        pass   
+
 
     def get_recommended_books(self):
+        # get the top 10 most popular authors
         pass
