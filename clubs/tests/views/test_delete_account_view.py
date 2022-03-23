9c0c71bb5efa7ebd8fa252828736ab360b57f7c2
Clara Zard <clara.zard@kcl.ac.uk>
2022-03-23 10:20:05 +0000

    Add tests to delete account view

--- clubs/tests/views/test_delete_account_view.py
+++ clubs/tests/views/test_delete_account_view.py
@@ -3,3 +3,20 @@ from clubs.models import User
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib import messages
+
+class DeleteAccountViewTestCase(TestCase):
+    fixtures = [
+        'clubs/tests/fixtures/default_user.json',
+        'clubs/tests/fixtures/other_users.json',
+        'clubs/tests/fixtures/default_calendar.json',
+        'clubs/tests/fixtures/default_club.json',
+        'clubs/tests/fixtures/default_rules.json',
+    ]
+
+    def setUp(self):
+        self.url = reverse('delete_account')
+        self.user = User.objects.get(email='johndoe@example.org')
+
+    def test_delete_account_url(self):
+        self.assertEqual(self.url, '/delete_account/')
