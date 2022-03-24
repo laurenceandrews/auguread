927dc6171e8dca0b63e81ca6c1f400550b681f64
k20065945-suryabrunton <surya.brunton@kcl.ac.uk>
2022-03-24 20:35:08 +0000

    Reset styles.css

--- bookclub/settings.py
+++ bookclub/settings.py
@@ -162,8 +162,8 @@ INTERNAL_IPS = [
 
 # Set up for windows, for mac/ubuntu change to r"/usr/local/bin/npm"
 # NPM_BIN_PATH = r"/usr/local/bin/npm"
-# NPM_BIN_PATH = "/usr/bin/npm"
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+NPM_BIN_PATH = "/usr/bin/npm"
+# NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 
 # Page lengths
