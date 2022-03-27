5e41ae99c6a85dff8f0807183aa254404f011261
k20065945-suryabrunton <surya.brunton@kcl.ac.uk>
2022-03-27 16:52:51 +0100

    Fix user_detail_view test

--- clubs/tests/views/test_user_detail_view.py
+++ clubs/tests/views/test_user_detail_view.py
@@ -1,5 +1,5 @@
 """Unit tests for the user detail."""
-from clubs.models import Club
+from clubs.models import Club, User
 from django.contrib import messages
 from django.test import TestCase
 from django.urls import reverse
@@ -9,17 +9,25 @@ from schedule.models import Calendar, Event, Rule
 class UserDetailViewTestCase(TestCase):
     fixtures = [
         'clubs/tests/fixtures/default_user.json',
+        'clubs/tests/fixtures/other_users.json',
         'clubs/tests/fixtures/default_calendar.json',
         'clubs/tests/fixtures/default_club.json'
     ]
 
     def setUp(self):
-        self.club = Club.objects.get(id=6)
-        self.user = Club.objects.get(id=6).owner
+        self.current_user = User.objects.get(pk=1)
+        self.user = User.objects.get(pk=2)
         self.url = reverse('user_detail', kwargs={'user_id': self.user.id})
 
-    def test_user_detail(self):
-        self.client.login(email=self.user.email, password="Password123")
+    def test_user_detail_for_current_user(self):
+        self.client.login(email=self.current_user.email, password="Password123")
+        url = reverse('user_detail', kwargs={'user_id': self.current_user.id})
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_detail.html')
+
+    def test_user_detail_for_other_users(self):
+        self.client.login(email=self.current_user.email, password="Password123")
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'user_detail.html')
@@ -29,6 +37,3 @@ class UserDetailViewTestCase(TestCase):
         url = reverse('user_detail', kwargs={'user_id': 0})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
-        # messages_list = list(response.context['messages'])
-        # self.assertEqual(len(messages_list), 1)
-        # self.assertEqual(messages_list[0].level, messages.ERROR)
