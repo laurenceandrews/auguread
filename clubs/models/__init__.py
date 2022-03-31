079f952d2e511927d6278cbe268b87883fd7f1f6
k20065945-suryabrunton <surya.brunton@kcl.ac.uk>
2022-03-31 23:20:34 +0100

    Create UserBookRecommendation model

--- clubs/models/__init__.py
+++ clubs/models/__init__.py
@@ -6,4 +6,4 @@ from .recommender_models import ClubBookRecommendation, MyUUIDModel
 from .scheduler_models import Address, MeetingAddress, MeetingLink
 from .user_models import (Book_Rating, Club, Club_Book_History, Club_Books,
                           Club_Users, User, User_Book_History, User_Books,
-                          UserManager)
+                          UserBookRecommendation, UserManager)
